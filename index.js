08675ae2091ccf74c39da6d0659f7594cb99e680
bahaeddine <azouzbahaeddine@isimm.u-monastir.tn>
2025-04-15 12:05:32 +0100

    adding the database connection and the crud to the database

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
+require('dotenv').config() //to acces the .env where we will find the env variable
 const express = require('express')
-const cors = require('cors')
 
 const app = express()
 app.use(express.static('dist'))
-app.use(cors())
 
 const requestLogger = (request, response, next) =>{
     console.log('Methode:',request.method)
@@ -16,75 +15,69 @@ const requestLogger = (request, response, next) =>{
 app.use(express.json())
 app.use(requestLogger)
 
-let notes = [
-    {
-        id: "1",
-        content: "HTML is easy",
-        important: true
-      },
-      {
-        id: "2",
-        content: "Browser can execute only JavaScript",
-        important: false
-      },
-      {
-        id: "3",
-        content: "GET and POST are the most important methods of HTTP protocol",
-        important: true
-      }
-]
-
-
+const Note = require('./models/note')
 
 
 app.get('/api/notes',(request, response)=>{
-    response.json(notes)
+    
+    Note.find({}).then(notes=>{
+        response.json(notes) 
+    })
 })
 
-app.get('/api/notes/:id',(request,reponse)=>{
-    const id = request.params.id
-    const note = notes.find(note=>note.id===id)
-    if(note){
-        reponse.json(note)
-    }else{
-        reponse.status(404).end()
-    }
+app.get('/api/notes/:id',(request,response,next)=>{
+   Note.findById(request.params.id).then(note=>{
+        if(note) {
+            response.json(note)
+        } else{
+            response.status(404).end()
+        }
+   })
+   .catch(error=> next(error))
     
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    console.log(id)
-    notes = notes.filter(note => note.id !== id)
-  
-    response.status(204).end()
-  })
-
-  const generateId =(()=>{
-    const maxId = notes.length>0
-    ?Math.max(...notes.map(n=>Number(n.id)))
-    :0
-    return String(maxId +1)
-  })
-
-
-app.post('/api/notes', (request, response)=>{
-    const body = request.body
-    if(!body.content){
-        return response.status(400).json({
-            error : 'contetn missing'
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id)
+        .then(result=>{
+            response.status(204).end() //status 204:no content
         })
-    }
-    
-    const note ={
-        content: body.content,
-        import	: body.important || false,
-        id: generateId()
-    }
-    
-    notes = notes.concat(note)
-    response.json(note)
+        .catch(error=> next(error))
+  })
+
+
+app.post('/api/notes', (request, response, next) => {
+    const body = request.body
+  
+    const note = new Note({
+      content: body.content,
+      important: body.important || false,
+    })
+  
+    note.save()
+      .then(savedNote => {
+        response.json(savedNote)
+      })
+      .catch(error => next(error))
+  })
+
+app.put('/api/notes/:id', (request, response, next)=>{
+    const{content, important } = request.body
+    Note.findById(request.params.id)
+        .then(note =>{
+            if(!note){
+                return response.status(404).end()
+            }
+
+            note.content = content
+            note.important = important
+
+            return note.save().then(updatedNote=>{
+                response.json(updatedNote)
+            })
+        }).catch(error=>next(error))
 })
+
 
 const unkownEndPoint = (request, response)=>{
     response.status(404).send({error: 'unkown endpoint'})
@@ -92,7 +85,24 @@ const unkownEndPoint = (request, response)=>{
 app.use(unkownEndPoint)
 
 
-const PORT = process.env.PORT || 3001
+const errorHandler = (error, request, response, next)=>{
+    console.error(error.message)
+
+    if(error.name === 'CastError') {
+        return response.status(400).send({ error : "malformatted id"})
+    }else if(error.name === 'ValidationError') {
+        return response.status(400).json({error: error.message})
+    }
+
+    next(error)
+}
+
+
+
+app.use(errorHandler)
+
+
+const PORT = process.env.PORT
 app.listen(PORT,()=>{
-    console.log(`server is running on port ${PORT}`)
+    console.log(`Server is running on port ${PORT}`)
 })
